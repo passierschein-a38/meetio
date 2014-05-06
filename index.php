9cecb0025fbdbf0c214899744577ac29fc9ba838
sauerhoefer <sauerhoefer@shop-werkstatt.com>
2014-05-06 11:03:14 +0200

    fix js errors

--- index.php
+++ index.php
@@ -16,13 +16,18 @@ if( !window.location.search.length ){
 
 var fb_key = 'sizzling-fire-7348'; //dev - production over heroku
 var base_url = 'http://localhost/meetio/';
+
 <?php
+
+$_ENV['FIREBASE'] = 'sizzling-fire-7348';
+$_ENV['BASE_URL'] = 'http://localhost/meetio/';
+
 if( isset( $_ENV['FIREBASE'] ) ){
-	echo sprintf( "fb_key='%s'", $_ENV['FIREBASE'] );
+	echo sprintf( "fb_key='%s';" . PHP_EOL, $_ENV['FIREBASE'] );
 }
 
 if( isset( $_ENV['BASE_URL'] ) ){
-	echo sprintf( "base_url='%s'", $_ENV['base_url'] );
+	echo sprintf( "base_url='%s';" . PHP_EOL, $_ENV['BASE_URL'] );
 }
 
 ?>
