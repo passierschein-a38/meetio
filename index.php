70a27e83ec6ceb13194b92a6f1e9e5c1efec32a4
sauerhoefer <sauerhoefer@shop-werkstatt.com>
2014-05-06 11:05:47 +0200

    wow so confused today

--- index.php
+++ index.php
@@ -18,10 +18,6 @@ var fb_key = 'sizzling-fire-7348'; //dev - production over heroku
 var base_url = 'http://localhost/meetio/';
 
 <?php
-
-$_ENV['FIREBASE'] = 'sizzling-fire-7348';
-$_ENV['BASE_URL'] = 'http://localhost/meetio/';
-
 if( isset( $_ENV['FIREBASE'] ) ){
 	echo sprintf( "fb_key='%s';" . PHP_EOL, $_ENV['FIREBASE'] );
 }
@@ -29,7 +25,6 @@ if( isset( $_ENV['FIREBASE'] ) ){
 if( isset( $_ENV['BASE_URL'] ) ){
 	echo sprintf( "base_url='%s';" . PHP_EOL, $_ENV['BASE_URL'] );
 }
-
 ?>
 
 var fb = new Firebase( 'https://' + fb_key + '.firebaseio.com/sessions/' );		
