10437685b72431bcdfe4d8397ce9d9828ea21e58
sauerhoefer <sauerhoefer@shop-werkstatt.com>
2014-05-06 10:58:33 +0200

    Added handling for config vars

--- index.php
+++ index.php
@@ -5,23 +5,37 @@
 <script type='text/javascript' src='functions.js'></script>
 </head>
 <body>
+
 <script type="text/javascript">		
 if( !window.location.search.length ){		
 	window.location.search = guid();
 }
 </script>
 
-
 <script>
-var fb = new Firebase( 'https://sizzling-fire-7348.firebaseio.com/sessions/' );		
+
+var fb_key = 'sizzling-fire-7348'; //dev - production over heroku
+var base_url = 'http://localhost/meetio/';
+<?php
+if( isset( $_ENV['FIREBASE'] ) ){
+	echo sprintf( "fb_key='%s'", $_ENV['FIREBASE'] );
+}
+
+if( isset( $_ENV['BASE_URL'] ) ){
+	echo sprintf( "base_url='%s'", $_ENV['base_url'] );
+}
+
+?>
+
+var fb = new Firebase( 'https://' + fb_key + '.firebaseio.com/sessions/' );		
 
 fb.child( window.location.search ).once( 'value', function( snapshot ) {
-
+	
 	//session does not exists
 	if( snapshot.val() === null ){
-		window.location.href = 'http://localhost/webrtc/multi_fastsupport/caller.html' + window.location.search;
+		window.location.href = base_url + 'caller.html' + window.location.search;
 	}else{
-		window.location.href = 'http://localhost/webrtc/multi_fastsupport/callee.html' + window.location.search;
+		window.location.href = base_url + 'callee.html' + window.location.search;
 	}
 });
 </script>
